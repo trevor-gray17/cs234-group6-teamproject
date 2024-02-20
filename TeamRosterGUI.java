e474c3e0b04412bc0bb3e7e06b6cd9168e19afdc
Trevor Gray <trevormgray@gmail.com>
2024-02-20 12:26:01 -0500

    Updated GUI to have tabs

--- TeamRosterGUI.java
+++ TeamRosterGUI.java
@@ -15,19 +15,27 @@ public class TeamRosterGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
         frame.setLayout(new BorderLayout());
-
-        teamListModel = new DefaultListModel<>();
-        teamList = new JList<>(teamListModel);
-        frame.add(new JScrollPane(teamList), BorderLayout.CENTER);
-
-        JPanel controlPanel = createControlPanel();
-        frame.add(controlPanel, BorderLayout.SOUTH);
-
+    
+        // Create a JTabbedPane
+        JTabbedPane tabbedPane = new JTabbedPane();
+    
+        // Create tabs for different information
+        JPanel playerPanel = editRosterPanel();
+        JPanel statisticsPanel = createStatisticsPanel();
+    
+        // Add tabs to the tabbedPane
+        tabbedPane.addTab("Roster", playerPanel);
+        tabbedPane.addTab("Statistics", statisticsPanel);
+    
+        frame.add(tabbedPane, BorderLayout.CENTER);
+    
         frame.setVisible(true);
     }
-
-    private JPanel createControlPanel() {
+    
+    
+    private JPanel editRosterPanel() {
         JPanel panel = new JPanel();
+        // Add components related to player information
         JTextField memberField = new JTextField(10);
         JButton addButton = new JButton("Add");
         JButton editButton = new JButton("Edit");
@@ -36,16 +44,27 @@ public class TeamRosterGUI {
         // The functionality of the buttons for the players will use the basketball player class
         // The functionality of the buttons for the shooting statistics will use the shooting statistics class
         // The two classes mentioned will need more improvements to be able to use the buttons
-        
+
         panel.add(memberField);
         panel.add(addButton);
         panel.add(editButton);
         panel.add(deleteButton);
+        return panel;
+    }
+    
+    private JPanel createStatisticsPanel() {
+        JPanel panel = new JPanel();
+        // Add components related to statistics information
+        // The functionality of the buttons for the players will use the basketball player class
+        // The functionality of the buttons for the shooting statistics will use the shooting statistics class
+        // The two classes mentioned will need more improvements to be able to use the buttons
 
         return panel;
     }
+    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(TeamRosterGUI::new);
     }
+    
 }
