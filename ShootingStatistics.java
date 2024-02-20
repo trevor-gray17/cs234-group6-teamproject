a78602763db2832a6298c6dc38578eb5630d7dbb
Trevor Gray <trevormgray@gmail.com>
2024-02-20 10:52:39 -0500

    created the shooting statistics

---
+++ ShootingStatistics.java
@@ -0,0 +1,22 @@
+class ShootingStatistics {
+    private double fieldGoalPercentage;
+    private double threePointPercentage;
+    private double freeThrowPercentage;
+
+    public ShootingStatistics(double fieldGoalPercentage, double threePointPercentage, double freeThrowPercentage) {
+        this.fieldGoalPercentage = fieldGoalPercentage;
+        this.threePointPercentage = threePointPercentage;
+        this.freeThrowPercentage = freeThrowPercentage;
+    }
+
+    // Getters and setters
+
+    @Override
+    public String toString() {
+        return "ShootingStatistics{" +
+                "fieldGoalPercentage=" + fieldGoalPercentage +
+                ", threePointPercentage=" + threePointPercentage +
+                ", freeThrowPercentage=" + freeThrowPercentage +
+                '}';
+    }
+}
