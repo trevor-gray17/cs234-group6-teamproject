6f4038b5067022035407475ac0be79880c678e00
Madison <20nasdgirl@gmail.com>
2024-03-14 10:19:09 -0400

    database create

---
+++ database.sql
@@ -0,0 +1,10 @@
+Create database if not exists shootingstats;
+Create table if not exists Player (
+    String name,
+    int number,
+    String position
+    Boolean active,
+    int year,
+);
+Create table if not exists Stats_1 (int threePointersTaken,int threePointersMade, int freeThrowsTaken , Select name from Player , Date practice_date);
+
