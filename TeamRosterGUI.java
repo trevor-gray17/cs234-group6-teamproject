a6808f9b04cd49adc0dc76b3397be64c5a16ee3c
Jesse Russo <russoj03@moravian.edu>
2024-03-28 09:40:42 -0400

    ComboBox

--- TeamRosterGUI.java
+++ TeamRosterGUI.java
@@ -7,9 +7,7 @@ import javax.swing.table.DefaultTableModel;
 public class TeamRosterGUI {
     private JFrame frame;
     private JTable PlayerTable;
-    private JTable PlayerStatsTable;
-    private JTable ByDateTable;
-
+    private JTable StatisticsTable;
 
     private String[][] playerList;
     private String[][] playerStats;
@@ -82,17 +80,17 @@ public class TeamRosterGUI {
         for (int i = 1; i < 5; i++) {
             playerStats[0][i] = "";
         }
-        PlayerStatsTable = new JTable(playerStats, statColumnNames);
+        StatisticsTable = new JTable(playerStats, statColumnNames);
 
-        PlayerStatsTable.setBackground(Color.LIGHT_GRAY);
-        PlayerStatsTable.setFont(new Font("Times", Font.PLAIN, 25));
-        PlayerStatsTable.setForeground(Color.BLACK);
-        PlayerStatsTable.setRowHeight(30);
-        PlayerStatsTable.setCellSelectionEnabled(true);
+        StatisticsTable.setBackground(Color.LIGHT_GRAY);
+        StatisticsTable.setFont(new Font("Times", Font.PLAIN, 25));
+        StatisticsTable.setForeground(Color.BLACK);
+        StatisticsTable.setRowHeight(30);
+        StatisticsTable.setCellSelectionEnabled(true);
 
-        JScrollPane statsPane = new JScrollPane(PlayerStatsTable);
+        JScrollPane statsPane = new JScrollPane(StatisticsTable);
 
-        tabbedPane.addTab("Player Stats", statsPane);
+        tabbedPane.addTab("Statistics", statsPane);
 
         // Overall Statistics Tab
         frame.add(tabbedPane, BorderLayout.CENTER);
@@ -102,9 +100,8 @@ public class TeamRosterGUI {
         frame.setVisible(true);
 
 
-        JScrollPane datesPane = new JScrollPane(ByDateTable);
-
-        tabbedPane.addTab("Stats by Date", datesPane);
+        // Dropdown for player selection in Statistics
+        final JComboBox<String> cb = new JComboBox<String>();
     }
     
 
@@ -159,7 +156,7 @@ public class TeamRosterGUI {
 
             for (int i = 0; i < playerStats.length; i++) {
                 for (int j = 1; j < 5; j++) {
-                    playerStats[i][j] = PlayerStatsTable.getValueAt(i, j).toString();
+                    playerStats[i][j] = StatisticsTable.getValueAt(i, j).toString();
             }}
 
             for (int i = 0; i < playerStats.length; i++) {
@@ -183,6 +180,11 @@ public class TeamRosterGUI {
 
 
             PlayerTable.setModel(new DefaultTableModel(playerList, new String[]{"Name", "Number", "Position", "Year", "Active"}));
+            PlayerTable.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(positionComboBox));
+            PlayerTable.getColumnModel().getColumn(4).setCellEditor(new DefaultCellEditor(activeComboBox));
+
+
+
             StatisticsTable.setModel(new DefaultTableModel(playerStats, new String[]{"Name", "Free Throws Made", "Free Throws Attempted", 
             "Three Pointers Made", "Three Pointers Attempted"}));
 
@@ -209,7 +211,7 @@ public class TeamRosterGUI {
 
             for (int i = 0; i < playerStats.length; i++) {
                 for (int j = 1; j < 5; j++) {
-                    playerStats[i][j] = PlayerStatsTable.getValueAt(i, j).toString();
+                    playerStats[i][j] = StatisticsTable.getValueAt(i, j).toString();
                     if(playerStats[i][j].toString() == "")
                         playerStats[i][j] = "NA";
                 }
@@ -221,6 +223,11 @@ public class TeamRosterGUI {
 
         
             PlayerTable.setModel(new DefaultTableModel(playerList, new String[]{"Name", "Number", "Position", "Year", "Active"}));
+
+            PlayerTable.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(positionComboBox));
+            PlayerTable.getColumnModel().getColumn(4).setCellEditor(new DefaultCellEditor(activeComboBox));
+
+
             StatisticsTable.setModel(new DefaultTableModel(playerStats, new String[]{"Name", "Free Throws Made", "Free Throws Attempted", 
             "Three Pointers Made", "Three Pointers Attempted"}));
 
@@ -233,13 +240,13 @@ public class TeamRosterGUI {
 
         deleteButton.addActionListener(e -> {
             int playerRow = PlayerTable.getSelectedRow();
-            int statsRow = PlayerStatsTable.getSelectedRow();
+            int statsRow = StatisticsTable.getSelectedRow();
             
             if (playerRow >= 0 || statsRow >= 0) {
                 int confirm = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this player and their statistics?", "Confirm Deletion", JOptionPane.YES_NO_OPTION);
                 if (confirm == JOptionPane.YES_OPTION) {
                     DefaultTableModel playerTableModel = (DefaultTableModel) PlayerTable.getModel();
-                    DefaultTableModel statsTableModel = (DefaultTableModel) PlayerStatsTable.getModel();
+                    DefaultTableModel statsTableModel = (DefaultTableModel) StatisticsTable.getModel();
                     
                     if (playerRow >= 0) {
                         playerTableModel.removeRow(playerRow);
